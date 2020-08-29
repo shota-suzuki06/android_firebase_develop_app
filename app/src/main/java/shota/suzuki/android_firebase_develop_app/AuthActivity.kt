807461524b432141e69c12aca397656b1cca581f
shota suzuki <shota.suzuki@gmail.com>
2020-08-29 23:55:02 +0900

    first commit

---
+++ app/src/main/java/shota/suzuki/android_firebase_develop_app/AuthActivity.kt
@@ -0,0 +1,89 @@
+package shota.suzuki.android_firebase_develop_app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import shota.suzuki.android_firebase_develop_app.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_auth)
+
+        auth = Firebase.auth
+
+        // Setup
+        setup()
+    }
+
+    private fun setup() {
+        title = "Authentication"
+
+        binding.apply {
+          btnRegisterId.setOnClickListener {
+              if (editEmailId.text.isNotEmpty() && editPasswordId.text.isNotEmpty()) {
+                  auth.createUserWithEmailAndPassword(editEmailId.text.toString(), editPasswordId.text.toString()).addOnCompleteListener {
+                      if (it.isSuccessful) {
+                          showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                      } else {
+                          showAlert("ユーザ登録に失敗しました")
+                      }
+                  }
+
+              } else {
+                  showAlert("メールアドレス または パスワードを入力してください")
+              }
+          }
+        }
+
+        binding.apply {
+            btnLoginId.setOnClickListener {
+                if (editEmailId.text.isNotEmpty() && editPasswordId.text.isNotEmpty()) {
+                    auth.signInWithEmailAndPassword(editEmailId.text.toString(), editPasswordId.text.toString()).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        } else {
+                            showAlert("ログインに失敗しました")
+                        }
+                    }
+                } else {
+                    showAlert("メールアドレス または パスワードを入力してください")
+                }
+            }
+        }
+
+    }
+
+    private fun showAlert(errorMessage: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage(errorMessage)
+        builder.setPositiveButton("OK", null)
+        builder.create().show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/shota/suzuki/android_firebase_develop_app/HomeActivity.kt
@@ -0,0 +1,35 @@
+package shota.suzuki.android_firebase_develop_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
+
+enum class ProviderType {
+    BASIC
+}
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        // Setup
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        setUp(email ?: "", provider ?: "")
+    }
+
+    private fun setUp(email: String, provider: String) {
+        title = "home"
+        textEmailId.text = email
+        textProviderId.text = provider
+
+        btnLogoutId.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+    }
+}
